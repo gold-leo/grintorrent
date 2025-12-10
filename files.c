5fe323160e60f0478e93a21a44cedebc58f06e38
Goldman <goldmanleo@goedel.cs.grinnell.edu>
2025-12-10 15:41:55 -0600

    added comparisons

--- files.c
+++ files.c
@@ -10,11 +10,21 @@ int create_new_file_desc(char* file_path, file_desc_t* file_desc) {
     // return -1 if fail, 0 if succeed
 }
 
-
-int verfiy_file(char* file_path, file_desc_t* file_desc) {
+/*
+* Verify a file's status compared to a file_desc. 
+* Items compared:
+*  - File size
+*  - File name
+*  - File hash
+*  - Num of chunks
+*  - Num of 
+*/
+int verify_file(char* file_path, file_desc_t* file_desc, file_status* file_status) {
     // check the size
     // check the hashes
     // check the name
     // return information on hash success and failure
     // return what chunks are verified
 }
+
+
